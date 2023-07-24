07754eb75c53d0e52c1e35b2b54ec83ff0d9129e
Maxime Bonnin <maxime.bonnin@gmx.de>
2023-07-24 15:50:30 +0100

    test name

--- java-api/src/main/resources/data.sql
+++ java-api/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO owners (owner_id, name) VALUES (1, 'Selvyn');
+INSERT INTO owners (owner_id, name) VALUES (1, 'Yen');
 INSERT INTO owners (owner_id, name) VALUES (2, 'Martina');
 
 INSERT INTO dogs (dog_id, name, age, owner_id) VALUES (1, 'Einstein', 3, 1);
