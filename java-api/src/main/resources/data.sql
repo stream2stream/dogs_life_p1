8ff358b1633d8077e4874f51925a3585bef16495
Anca Maria <ciuciuancamaria@gmail.com>
2023-07-24 15:55:47 +0100

    Added insert queries for owners and dogs

---
+++ java-api/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO owners (owner_id, name) VALUES (1, 'Anca');
+INSERT INTO owners (owner_id, name) VALUES (2, 'Maria');
+
+INSERT INTO dogs (dog_id, name, age, owner_id) VALUES (1, 'Yan', 3, 1);
+INSERT INTO dogs (dog_id, name, age, owner_id) VALUES (2, 'Dodo', 5, 2);
