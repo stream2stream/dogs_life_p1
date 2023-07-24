3082e654b402b9596d66491a545d988a4a55d4b8
ispsv <94699010+ispsv@users.noreply.github.com>
2023-07-24 16:20:59 +0100

    Create data.sql

---
+++ java-api/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO owners (owner_id, name) VALUES (1, 'John Doe');
+INSERT INTO owners (owner_id, name) VALUES (2, 'Marie Doe');
+INSERT INTO owners (owner_id, name) VALUES (3, 'Any Name');
+
+INSERT INTO dogs (name, age, owner_id) VALUES ('Einstein', 2, 1);
+INSERT INTO dogs (name, age, owner_id) VALUES ('Rex', 5, 1);
+INSERT INTO dogs (name, age, owner_id) VALUES ('Lassy', 10, 3);
+INSERT INTO dogs (name, age, owner_id) VALUES ('Kaya', 7, 2);
