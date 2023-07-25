fe222e7b82f2bf92f46401b6d4cab04ff5be6d91
Dilshaan <dilshaan.grewal.001@gmail.com>
2023-07-25 10:30:29 +0100

    Add schema for owners and dogs table. Add example data for tables.

---
+++ java-api/src/main/resources/data.sql
@@ -0,0 +1,19 @@
+INSERT into owners
+    (name)
+values
+    ('Olivia'),
+    ('Noah'),
+    ('Emma'),
+    ('Liam')
+;
+
+
+INSERT into dogs
+    (name, age, owner_id)
+values
+    ('Luna', 5, 1),
+    ('Bella', 10, 2),
+    ('Lola', 15, 3),
+    ('Poppy', 2, 4),
+    ('Coco', 5, 1),
+;
---
+++ java-api/src/main/resources/schema.sql
@@ -0,0 +1,14 @@
+create table owners
+(
+    owner_id int AUTO_INCREMENT PRIMARY KEY,
+    name int not null
+);
+
+create table dogs
+(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    name varchar (50) not null,
+    age int not null,
+    owner_id int not null,
+    FOREIGN key (owner_id) REFERENCES owners (owner_id)
+);
