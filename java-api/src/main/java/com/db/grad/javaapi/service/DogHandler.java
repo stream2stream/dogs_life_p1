d41853e7315e1dafd97e4f8bc42e90511b61072b
Simon <simon.benkner@gmx.de>
2023-07-27 13:08:39 +0100

    Added DogsHandler Class with first test

---
+++ java-api/src/main/java/com/db/grad/javaapi/service/DogHandler.java
@@ -0,0 +1,22 @@
+package com.db.grad.javaapi.service;
+
+import com.db.grad.javaapi.model.Dog;
+import com.db.grad.javaapi.repository.DogsRepository;
+
+public class DogHandler {
+
+    private DogsRepository itsDogsRepo;
+
+    public DogHandler(DogsRepository itsDogRepo) {
+        this.itsDogsRepo = itsDogRepo;
+    }
+
+
+    public long addDog(Dog theDog) {
+        return itsDogsRepo.save(theDog);
+    }
+
+    public long getNoOfDogs() {
+        return itsDogsRepo.count();
+    }
+}
