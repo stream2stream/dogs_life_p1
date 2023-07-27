deded41a78da57c0df8c14fc9b03d36fd7f76470
ancamariac <ciuciuancamaria@gmail.com>
2023-07-27 16:13:49 +0300

    Writing code using TDD - QLC2

---
+++ java-api/src/test/java/com/db/grad/javaapi/service/DogsHandlerTest.java
@@ -0,0 +1,41 @@
+package com.db.grad.javaapi.service;
+
+import com.db.grad.javaapi.model.Dog;
+import com.db.grad.javaapi.repository.DogsRepository;
+import com.db.grad.javaapi.repository.DogsRepositoryStub;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DogsHandlerTest {
+
+    private DogsRepository itsDogRepo = new DogsRepositoryStub();
+
+    @BeforeEach
+    public void makeSureRepoIsEmpty() {
+        itsDogRepo.deleteAll();
+    }
+
+    @Test
+    public void addADogReturnNumberOfDogsInRepoIsOne(){
+
+        //arrange
+
+        DogHandler cut = new DogHandler(itsDogRepo);
+        Dog theDog = new Dog();
+        theDog.setName("Bruno");
+        cut.addDog( theDog );
+
+        int expectedResult = 1;
+
+        // act
+
+        long actualResult = cut.getNoOfDogs();
+
+        // assert
+
+        assertEquals( expectedResult, actualResult );
+    }
+
+}
