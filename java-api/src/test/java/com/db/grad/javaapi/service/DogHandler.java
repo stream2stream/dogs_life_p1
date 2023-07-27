e80e589ad7aaa6e268140c277628a509b71fbfb7
Jinyi <jinyi.jin@rwth-aachen.de>
2023-07-27 13:30:32 +0100

    add DogHandlerTest

---
+++ java-api/src/test/java/com/db/grad/javaapi/service/DogHandler.java
@@ -0,0 +1,20 @@
+package com.db.grad.javaapi.service;
+
+import com.db.grad.javaapi.model.Dog;
+import com.db.grad.javaapi.repository.DogsRepository;
+
+public class DogHandler {
+    private DogsRepository itsDogsRepo;
+    public DogHandler(DogsRepository repo) {
+        itsDogsRepo = repo;
+
+    }
+
+    public long addDog(Dog theDog) {
+        return itsDogsRepo.save(theDog);
+    }
+
+    public long getNoOfDogs() {
+        return itsDogsRepo.count();
+    }
+}
