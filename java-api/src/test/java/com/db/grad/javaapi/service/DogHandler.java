47d5158cc075c479d3a074c1df50274a48d77834
jinyi <jinyi.jin@rwth-aachen.de>
2023-07-27 15:59:35 +0100

    add methods in Doghandler

--- java-api/src/test/java/com/db/grad/javaapi/service/DogHandler.java
+++ java-api/src/test/java/com/db/grad/javaapi/service/DogHandler.java
@@ -3,6 +3,8 @@ package com.db.grad.javaapi.service;
 import com.db.grad.javaapi.model.Dog;
 import com.db.grad.javaapi.repository.DogsRepository;
 
+import java.util.Optional;
+
 public class DogHandler {
     private DogsRepository itsDogsRepo;
     public DogHandler(DogsRepository repo) {
@@ -17,4 +19,33 @@ public class DogHandler {
     public long getNoOfDogs() {
         return itsDogsRepo.count();
     }
+
+    public Dog getDogByName(Dog theDog){
+        int NoOfName = itsDogsRepo.findByName(theDog).size();
+        if (NoOfName == 1){
+            return theDog;
+        }
+        else{
+            return null;}
+    }
+
+    public Dog getDogById(long id){
+        return itsDogsRepo.findById(id);
+    }
+
+    long updateDogDetails(Dog dog){
+        return itsDogsRepo.save(dog);
+    }
+
+    boolean removeDog(long id){
+        boolean result =  false;
+        Dog toRemove = itsDogsRepo.findById(id);
+        if(toRemove != null){
+            itsDogsRepo.delete(toRemove);
+            result = true;
+        }
+        return result;
+    }
+
+
 }
