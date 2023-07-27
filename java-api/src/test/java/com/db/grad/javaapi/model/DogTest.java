f0d3fc13beb2928086b5dfca68bea6dbb14cfc56
Admin <cey.akbulut@gmail.com>
2023-07-27 16:30:33 +0100

    3rd commit-local

--- java-api/src/test/java/com/db/grad/javaapi/model/DogTest.java
+++ java-api/src/test/java/com/db/grad/javaapi/model/DogTest.java
@@ -1,5 +1,17 @@
 package com.db.grad.javaapi.model;
 
-class DogTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+class DogTest {
+    @Test
+    public void testUpdateDogDetails() {
+        Dog originalDog = new DogsHandler();
+        originalDog.setId(1);
+        Dog updatedDog = new DogsHandler();
+        updatedDog.setId(1);
+
+        long updatedDogId = originalDog.updateDogDetails(updatedDog);
+        Assertions.assertEquals(1, updatedDogId);
+    }
 }
