f587b61d6c0cf4ecd40c0cfc119a3cbb9fd7ff8e
Ashwin <Ashwinsuresh2013@gmail.com>
2023-07-27 19:33:10 +0100

    QLC 2

---
+++ java-api/src/test/java/com/db/grad/javaapi/service/DogsHandler.java
@@ -0,0 +1,31 @@
+package com.db.grad.javaapi.service;
+
+import com.db.grad.javaapi.model.Dog;
+import com.db.grad.javaapi.repository.DogsRepository;
+import com.db.grad.javaapi.repository.DogsRepositoryStub;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DogsHandler {
+
+    private DogsRepository itsDogRepo = new DogsRepositoryStub();
+    @BeforeEach
+    public void makeSureRepoIsEmpty()
+    {
+        itsDogRepo.deleteAll();
+    }
+    @Test
+    public void add_a_dog_return_number_of_dogs_in_repo_is_one()
+    {
+        DogHandler cut = new DogHandler(itsDogRepo);
+        Dog theDog = new Dog();
+        theDog.setName("Bruno");
+        cut.addDog( theDog );
+
+        long actualResult = cut.getNoOfDogs();
+        int expectedResult = 1;
+        assertEquals(expectedResult, actualResult);
+    }
+}
