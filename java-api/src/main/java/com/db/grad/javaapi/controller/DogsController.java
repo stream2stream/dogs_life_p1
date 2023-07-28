9ec3854c4009004fd7141b5845bddf2b5dbfe149
Mario <marioirimia@gmail.com>
2023-07-28 17:20:45 +0300

    Update the WelcomeController to DogsController
    Added a status and the name change

--- java-api/src/main/java/com/db/grad/javaapi/controller/DogsController.java
+++ java-api/src/main/java/com/db/grad/javaapi/controller/DogsController.java
@@ -4,11 +4,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class WelcomeController {
+public class DogsController {
 
   @GetMapping("/")
   public String getWelcome() {
     return "Dogs API is up and running!";
+  }
+
+  @GetMapping("/status")
+  public String getStatus(){
+    return "Dogs API is healthy!";
   }
 
 }
