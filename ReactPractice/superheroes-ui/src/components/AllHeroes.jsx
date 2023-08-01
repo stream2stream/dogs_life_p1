9cbf7ed76ac5ad4b9e930574fa1613fcbc6511c3
Stephen Oluwatunmise <12stephenbrowne@gmail.com>
2023-08-01 13:26:15 +0100

    Completed QL-6 (Router and Inverse Data Flow)

--- ReactPractice/superheroes-ui/src/components/AllHeroes.jsx
+++ ReactPractice/superheroes-ui/src/components/AllHeroes.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import HeroDetail from './HeroDetail'
+import Row from 'react-bootstrap/Row'
 
 const AllHeroes = () => {
   
@@ -17,12 +18,24 @@ const AllHeroes = () => {
         {"id":10,"name":"Dick Grayson","alias":"Nightwing","superpower":"Acrobatics","teamID":5}
         ]
   
+    const [total, setTotal] = useState(0);
 
+    const totalCountHandler = (name) =>{
+        console.log(name)
+        setTotal(total+1);
+    }
     return (
-        heroes.map(hero=>(
-            <HeroDetail info={hero} key={hero.id.toString()}/>
+        <>
+            <h1> Total count is: {total}</h1>
+            <Row>
+            {heroes.map(hero=>(
+                <div className='container' key={hero.id.toString()}>
+                <HeroDetail info={hero} parentCount={totalCountHandler}/>
+                </div>
 
-        ))
+            ))}
+            </Row>
+        </>
     
   )
 
--- ReactPractice/superheroes-ui/src/components/HeroDetail.jsx
+++ ReactPractice/superheroes-ui/src/components/HeroDetail.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
-import batman from '../images/batman.jpeg'
+import React, { useState } from 'react'
+import images from '..'
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+import Row from 'react-bootstrap/esm/Row'
+import Col from 'react-bootstrap/esm/Col'
+
 
 const HeroDetail = (props) => {
+
+    const[counter, setCounter] = useState(0);
+    const incrementCounter=()=>{
+        setCounter(counter+1);
+        props.parentCount(props.info.alias)
+    }
+    
   return (
-     <div>
-        <img src={batman} alt="Batman" height="250px"/>
-        <p id="blue-alias">Alias: {props.info.alias}</p>
-        <p class="green-class">Name: {props.info.name}</p>
-        <button class="green-class">Like</button>   
+    <Card>
+     <div className="imgContainer">
+        <Card.Img variant="top" src={images[props.info.alias.replaceAll(/\s/g,'')]} alt={props.info.name} height="250px"/>
     </div>
+    <Card.Body>
+        <Card.Title>Alias: {props.info.alias}</Card.Title>
+        <Card.Text>Name: {props.info.name}</Card.Text>
+        <Row>
+            <Col><Button variant="primary" onClick={incrementCounter}>Like</Button></Col>
+            <Col>{counter > 0 && <Card.Text >Likes: {counter}</Card.Text>}</Col>
+        </Row>
+        
+    </Card.Body>
+    
+    </Card>
   )
 }
 
